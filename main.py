89841b66dbd737b95f8740ab43c03cef83fe5705
lijing <lijing@gohighsec.com>
2017-12-24 14:29:45 +0800

    add continue trainning

--- main.py
+++ main.py
@@ -239,6 +239,28 @@ def run():
 
         # OPTIONAL: Apply the trained model to a video
 
+def continue_trainning():
+    sess=tf.Session()    
+    #First let's load meta graph and restore weights
+    saver = tf.train.import_meta_graph('.checkpoints/my_test_model-1000.meta')
+    saver.restore(sess,tf.train.latest_checkpoint('./'))
+
+    # Now, let's access and create placeholders variables and
+    # create feed-dict to feed new data
+
+    graph = tf.get_default_graph()
+    image_input = graph.get_tensor_by_name("image_input:0")
+    keep_prob = graph.get_tensor_by_name("keep_prob:0")
+    learning_rate = graph.get_tensor_by_name("learning_rate:0")
+    correct_label = graph.get_tensor_by_name("correct_label:0")
+    train_op = graph.get_operation_by_name("train_op")
+    cross_entropy_loss = graph.get_operation_by_name("cross_entropy_loss")
+    #feed_dict ={w1:13.0,w2:17.0}
+
+    #Now, access the op that you want to run. 
+    op_to_restore = graph.get_tensor_by_name("op_to_restore:0")
+
+    print sess.run(op_to_restore,feed_dict)
 
 if __name__ == '__main__':
     run()
